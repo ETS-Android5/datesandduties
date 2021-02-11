87b6ca0a2a421b88b45a8a280fe1f6e6adde4ad0
Saffron Edwards <saffrone@iastate.edu>
2021-02-10 18:22:17 -0600

    Username and password template to work on

---
+++ Experiments/saffrone/demoexperiment/app/src/main/java/com/example/demoexperiment/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.demoexperiment.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
