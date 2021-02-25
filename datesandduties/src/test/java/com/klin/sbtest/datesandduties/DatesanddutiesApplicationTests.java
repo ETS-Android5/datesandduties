5a146d01a69208b1c3ff68b83307a41e6903d35a
Kevin Lin <klin@iastate.edu>
2021-02-24 18:49:18 -0600

    moving files to branch

---
+++ datesandduties/src/test/java/com/klin/sbtest/datesandduties/DatesanddutiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klin.sbtest.datesandduties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatesanddutiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
