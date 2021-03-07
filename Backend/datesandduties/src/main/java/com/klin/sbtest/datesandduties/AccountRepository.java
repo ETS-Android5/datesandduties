bd33311744febdc25bc547279aaaf38265c56c07
Kevin Lin <klin@iastate.edu>
2021-03-06 19:04:37 -0600

    renamed some files

--- Backend/datesandduties/src/main/java/com/klin/sbtest/datesandduties/AccountRepository.java
+++ Backend/datesandduties/src/main/java/com/klin/sbtest/datesandduties/AccountRepository.java
@@ -1,7 +1,5 @@
 package com.klin.sbtest.datesandduties;
 import org.springframework.data.repository.CrudRepository;
-import com.klin.sbtest.datesandduties.Account;
-
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
