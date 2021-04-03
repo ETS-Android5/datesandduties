c40355c521519193a275bd06ed890a7de3d5c79c
Brady228 <brady228@iastate.edu>
2021-04-03 13:27:36 -0500

    push stuff to clear

--- Frontend/datesandduties/app/src/main/java/com/example/datesandduties/homePage.java
+++ Frontend/datesandduties/app/src/main/java/com/example/datesandduties/homePage.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.example.datesandduties.sign_in_page;
 
 public class homePage extends Activity implements View.OnClickListener {
 
     private Button signOut, goToDates;
+
+    String user = sign_in_page.getUsername();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
