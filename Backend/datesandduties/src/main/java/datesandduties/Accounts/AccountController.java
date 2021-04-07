25816ff451c0fbc06bd1f9031735e87fa372abaa
Kevin Lin <klin@iastate.edu>
2021-04-07 01:58:01 -0500

    Added some mockito tests

--- Backend/datesandduties/src/main/java/datesandduties/Accounts/AccountController.java
+++ Backend/datesandduties/src/main/java/datesandduties/Accounts/AccountController.java
@@ -30,13 +30,14 @@ import io.swagger.annotations.ApiResponse;
 @RestController
 @RequestMapping(path = "/account")
 public class AccountController {
-	@Autowired 
+
+	@Autowired
 	private AccountRepository accountRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private EventRepository eventRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private TaskRepository taskRepository;
 
 	@ApiOperation(value = "Adds an Account to the Database", response = Account.class, tags = "addNewAccount")
@@ -70,7 +71,7 @@ public class AccountController {
 	public Optional<Account> getAccountById(@PathVariable int id) {
 		return accountRepository.findById(id);
 	}
-	
+
 	@PutMapping(path = "/update/{id}")
 	public Account updateAccount(@PathVariable int id, @RequestBody Account request) {
 		Optional<Account> event = accountRepository.findById(id);
@@ -80,19 +81,18 @@ public class AccountController {
 		accountRepository.save(request);
 		return accountRepository.findById(id).get();
 	}
-	
+
 	@GetMapping(path = "/events/{id}")
-	public List <Event> accountEvents(@PathVariable int id){
+	public List<Event> accountEvents(@PathVariable int id) {
 		Account account = accountRepository.findById(id).get();
 		return account.getEvents();
 	}
-	
+
 	@GetMapping(path = "/tasks/{id}")
-	public List <Task> accountTasks(@PathVariable int id){
+	public List<Task> accountTasks(@PathVariable int id) {
 		Account account = accountRepository.findById(id).get();
 		return account.getTasks();
 	}
-	
 
 	@PutMapping(path = "/assignEvent/{accountId}/{eventId}")
 	public String addEventToAccount(@PathVariable int accountId, @PathVariable int eventId) {
@@ -107,7 +107,7 @@ public class AccountController {
 		eventRepository.save(event);
 		return "Success!";
 	}
-	
+
 	@PutMapping(path = "/assignTask/{accountId}/{taskId}")
 	public String addTaskToAccount(@PathVariable int accountId, @PathVariable int taskId) {
 		Account account = accountRepository.findById(accountId).get();
@@ -121,6 +121,7 @@ public class AccountController {
 		taskRepository.save(task);
 		return "Success!";
 	}
+
 	@PutMapping(path = "/removeEvent/{accountId}/{eventId}")
 	public String removeEvent(@PathVariable int accountId, @PathVariable int eventId) {
 		Account account = accountRepository.findById(accountId).get();
@@ -134,6 +135,7 @@ public class AccountController {
 		eventRepository.save(event);
 		return "Event Removed From Account!";
 	}
+
 	@PutMapping(path = "/removeTask/{accountId}/{eventId}")
 	public String removeTask(@PathVariable int accountId, @PathVariable int taskId) {
 		Account account = accountRepository.findById(accountId).get();
@@ -147,10 +149,10 @@ public class AccountController {
 		taskRepository.save(task);
 		return "Task Removed from Account!";
 	}
+
 	@GetMapping(path = "/findUser/{username}")
 	public Account searchByUsername(@PathVariable String username) {
 		return accountRepository.findByUsername(username);
 	}
-	
 
 }
---
+++ Backend/datesandduties/src/test/java/datesandduties/AccountControllerTests.java
@@ -0,0 +1,61 @@
+package datesandduties;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import datesandduties.Accounts.Account;
+import datesandduties.Accounts.AccountController;
+import datesandduties.Accounts.AccountRepository;
+
+public class AccountControllerTests {
+	
+	@Test
+	public void testAddNewAccount() {
+		Account account = new Account("name", "mockUsername", "password", "email", "gender", 15, 1234567890, "country");
+		AccountController testController = mock(AccountController.class);
+
+		when(testController.addNewAccount(account)).thenReturn("Entry Saved!");
+
+		String result = testController.addNewAccount(account);
+		assertEquals("Entry Saved!", result);
+
+		verify(testController, times(1)).addNewAccount(account);
+
+	}/*
+
+	@InjectMocks
+	AccountController acctController;
+
+	@Mock
+	AccountRepository accountRepository;
+
+	@Test
+	public void testFindByUsername() { 
+		
+		when(accountRepository.findByUsername("mockUsername")).thenReturn(new Account("name", "mockUsername", "password", "email", "gender", 15, 1234567890, "country"));
+
+		Account account = acctController.searchByUsername("mockUsername");
+
+		assertEquals("name", account.getName());
+		assertEquals("mockUsername", account.getUsername());
+		assertEquals("password", account.getPassword());
+		assertEquals("email", account.getEmail());
+		assertEquals("gender", account.getGender());
+		assertEquals((Integer) 15, account.getAge());
+		assertEquals((Integer) 1234567890, account.getPhone());
+		assertEquals("country", account.getCountry());
+	}*/
+
+
+
+}
