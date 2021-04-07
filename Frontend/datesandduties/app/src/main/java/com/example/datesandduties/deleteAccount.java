0a4ad488984f93b071a4717a10e5194ca3c967cf
Saffron Edwards <saffrone@iastate.edu>
2021-04-06 21:51:53 -0500

    Mockito libraries added to gradle, ready to write tests

---
+++ Frontend/datesandduties/app/src/main/java/com/example/datesandduties/deleteAccount.java
@@ -0,0 +1,31 @@
+package com.example.datesandduties;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Request.Method;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.example.datesandduties.app.AppController;
+import com.example.datesandduties.net_utils.Const;
+import com.example.datesandduties.MainActivity;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class deleteAccount extends Activity{
+
+}
