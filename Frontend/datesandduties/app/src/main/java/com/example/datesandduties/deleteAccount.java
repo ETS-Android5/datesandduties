557a282a6f17103d11ff99f835bbe474c93e2f66
Saffron Edwards <saffrone@iastate.edu>
2021-04-07 15:13:24 -0500

    More delete account changes

--- Frontend/datesandduties/app/src/main/java/com/example/datesandduties/deleteAccount.java
+++ Frontend/datesandduties/app/src/main/java/com/example/datesandduties/deleteAccount.java
@@ -1,31 +1,20 @@
 package com.example.datesandduties;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.Request.Method;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.example.datesandduties.app.AppController;
 import com.example.datesandduties.net_utils.Const;
-import com.example.datesandduties.MainActivity;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class deleteAccount extends Activity {
 
@@ -37,14 +26,12 @@ public class deleteAccount extends Activity {
 
     private String TAG = sign_in_page.class.getSimpleName();
     //private String login_string_req = "string_req";
-    private String accept;
 
-    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_delete_account);
+        setContentView(R.layout.activity_setting);
 
         deleteAct = (Button) findViewById(R.id.delAct);
         //test = (TextView) findViewById(R.id.loginDebug);
@@ -58,7 +45,6 @@ public class deleteAccount extends Activity {
 
     private void deleteCheck() {
 
-        //check login using login volley get
         String url = Const.DEL_USER + "/" + sign_in_page.getID();
 
         StringRequest del = new StringRequest(Method.DELETE, url,
@@ -77,37 +63,7 @@ public class deleteAccount extends Activity {
         });
 
 
-       /* loginUsername = (EditText) findViewById(R.id.loginUsername);
-        loginPassword = (EditText) findViewById(R.id.loginPassword);
-
-        String url = Const.URL_LOGIN + "/" + loginUsername.getText().toString() + "/" + loginPassword.getText().toString();
-        StringRequest signon = new StringRequest(Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d(TAG, response.toString());
-                        accept = response.toString();
-
-                        if(accept.equals("Login successful!")){
-                            //do stuff
-                            test.setText(accept);
-                            username = loginUsername.getText().toString();
-                            startActivity(new Intent(deleteAccount.this, homePage.class));
-                        }
-                        else{
-                            //login incorrect do nothing
-                            test.setText(accept);
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: "+ error.getMessage());
-            }
-        });*/
-
-        //AppController.getInstance().addToRequestQueue(signon);
+        AppController.getInstance().addToRequestQueue(del);
 
 
 
