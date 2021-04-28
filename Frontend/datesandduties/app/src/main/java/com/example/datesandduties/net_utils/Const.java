df999e89a5c53e625f9d4fe4e03c87ebd015e61b
Saffron Edwards <saffrone@iastate.edu>
2021-04-27 23:52:45 -0500

    Add task beginnings

--- Frontend/datesandduties/app/src/main/java/com/example/datesandduties/net_utils/Const.java
+++ Frontend/datesandduties/app/src/main/java/com/example/datesandduties/net_utils/Const.java
@@ -18,4 +18,6 @@ public class Const {
     public static final String LINK_EVENT = "http://coms-309-008.cs.iastate.edu:8080/account/assignEvent"; //accid //eventid
     public static final String GET_EVENTS = "http://coms-309-008.cs.iastate.edu:8080/account/events"; //id
 
+    public static final String ADD_TASK = "http://coms-309-008.cs.iastate.edu:8080/task/add"; //add task
+
 }
