9801845c34122bef822fc797660723d5f553c74e
Kevin Lin <klin@iastate.edu>
2021-04-28 16:38:26 -0500

    Added more features to the websocket. Can now pull up history by inputting HISTORY, can delete all messages with DELETEALL, can delete your messages with DELETEME

--- Backend/datesandduties/src/main/java/datesandduties/Messages/MessageRepository.java
+++ Backend/datesandduties/src/main/java/datesandduties/Messages/MessageRepository.java
@@ -1,7 +1,16 @@
 package datesandduties.Messages;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface MessageRepository extends JpaRepository<Message, Integer> {
    public Message findByUsername(String username);
+   
+   @Modifying
+   @Query(value = "DELETE FROM Message e WHERE e.username = :username")
+   public void deleteByUsername(@Param("username")String username);
 }
