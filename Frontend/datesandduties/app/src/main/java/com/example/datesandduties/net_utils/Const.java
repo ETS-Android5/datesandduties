cc22b619dd01b7eca7d48438cbf3eb029d07ce0e
Brady228 <brady228@iastate.edu>
2021-04-28 12:42:57 -0500

    allowed user to view events and submit new events as well as cycle through calendar days and edit and delete events.

--- Frontend/datesandduties/app/src/main/java/com/example/datesandduties/net_utils/Const.java
+++ Frontend/datesandduties/app/src/main/java/com/example/datesandduties/net_utils/Const.java
@@ -6,16 +6,17 @@ public class Const {
     public static final String URL_CREATE_ACCOUNT = "http://coms-309-008.cs.iastate.edu:8080/account/add";
     public static final String URL_LOGIN = "http://coms-309-008.cs.iastate.edu:8080/account/login";
     public static final String DEL_USER = "http://coms-309-008.cs.iastate.edu:8080/account/delete";
+    public static final String REM_EVENT = "http://coms-309-008.cs.iastate.edu:8080/account/removeEvent";
+    public static final String FIND_USER = "http://coms-309-008.cs.iastate.edu:8080/account/findUser"; //username
+    public static final String LINK_EVENT = "http://coms-309-008.cs.iastate.edu:8080/account/assignEvent"; //accid //eventid
+    public static final String GET_EVENTS = "http://coms-309-008.cs.iastate.edu:8080/account/events"; //id
 
 
     public static final String ADD_EVENT = "http://coms-309-008.cs.iastate.edu:8080/event/add";
     public static final String ALL_EVENT = "http://coms-309-008.cs.iastate.edu:8080/event/all";
     public static final String DEL_EVENT = "http://coms-309-008.cs.iastate.edu:8080/event/delete";
-
-    public static final String FIND_USER = "http://coms-309-008.cs.iastate.edu:8080/account/findUser"; //username
     public static final String FIND_EVENT = "http://coms-309-008.cs.iastate.edu:8080/event/findEvent"; //title
+    public static final String EDIT_EVENT = "http://coms-309-008.cs.iastate.edu:8080/event/update";
 
-    public static final String LINK_EVENT = "http://coms-309-008.cs.iastate.edu:8080/account/assignEvent"; //accid //eventid
-    public static final String GET_EVENTS = "http://coms-309-008.cs.iastate.edu:8080/account/events"; //id
 
 }
